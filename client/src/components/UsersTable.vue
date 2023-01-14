17bdec39279650269379aeed504a1405b48b4282
olegpaliy <goleh@ukr.net>
2023-01-14 14:15:24 +0200

    edit fix

--- client/src/components/UsersTable.vue
+++ client/src/components/UsersTable.vue
@@ -7,6 +7,7 @@
             <v-card-title> My CRUD </v-card-title>
             <v-card-text>
               <v-data-table
+                fixed-headers
                 :headers="headers"
                 :items="inputAccounts"
                 class="elevation-10"
@@ -15,9 +16,7 @@
               >
                 <template v-slot:[`item.actions`]="{ item }">
                   <v-btn icon color="primary">
-                    <v-icon class="mr-2" @click="editUser(item)">
-                      mdi-pencil
-                    </v-icon>
+                    <v-icon @click="editUser(item)"> mdi-pencil </v-icon>
                   </v-btn>
                   <v-btn icon color="red darken-2">
                     <v-icon @click="deleteItem(item.id)"> mdi-delete </v-icon>
@@ -38,40 +37,29 @@
                   </v-container>
                 </template>
               </v-data-table>
-
-              <v-dialog v-model="userDialog" width="500px">
-                <v-alert
-                  v-model="validForm"
-                  border="top"
-                  colored-border
-                  type="info"
-                  elevation="2"
-                >
-                  Please enter a valid information
-                </v-alert>
+              <v-dialog v-model="checkUserDialog" width="500px">
                 <v-card>
                   <v-card-title></v-card-title>
                   <v-card-text>
                     <v-row>
                       <v-col cols="12">
-                        <v-text-field label="Login" v-model="editedItem.login">
-                        </v-text-field>
+                        <v-text-field
+                          label="Login"
+                          v-model="editedItem.login"
+                        ></v-text-field>
                         <v-text-field
                           label="First Name"
                           v-model="editedItem.firstName"
-                        >
-                        </v-text-field>
+                        ></v-text-field>
                         <v-text-field
                           label="Last Name"
                           v-model="editedItem.lastName"
-                        >
-                        </v-text-field>
+                        ></v-text-field>
                         <v-text-field
                           :rules="emailRules"
                           label="E-mail"
                           v-model="editedItem.email"
-                        >
-                        </v-text-field>
+                        ></v-text-field>
                       </v-col>
                     </v-row>
                   </v-card-text>
@@ -79,9 +67,8 @@
                   <v-card-actions>
                     <v-btn
                       @click="
-                        userDialog = false;
+                        checkUserDialog = false;
                         isCreating = false;
-                        validForm = false;
                       "
                       color="secondary"
                       class="mb-2"
@@ -89,7 +76,12 @@
                       Cancel
                     </v-btn>
                     <v-spacer></v-spacer>
-                    <v-btn @click="isCreateOrEdit" color="primary" class="mb-2">
+                    <v-btn
+                      @click="isCreateOrEdit"
+                      :disabled="!validFormDialog"
+                      color="primary"
+                      class="mb-2"
+                    >
                       Save
                     </v-btn>
                   </v-card-actions>
@@ -150,14 +142,35 @@ export default {
         return this.accounts;
       },
     },
+    validFormDialog() {
+      return (
+        this.editedItem.login &&
+        this.editedItem.firstName &&
+        this.editedItem.lastName &&
+        this.editedItem.email &&
+        this.pattern.test(this.editedItem.email)
+      );
+    },
+    checkUserDialog: {
+      get() {
+        return this.userDialog;
+      },
+      set(val) {
+        if (!val) {
+          this.editedItem = this.defaultItem;
+        }
+        this.userDialog = val;
+      },
+    },
   },
   data() {
     return {
       userDialog: false,
       userDelete: false,
-      validForm: false,
       deletedId: null,
       isCreating: false,
+      pattern:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 
       headers: [
         { text: "ID", value: "id", divider: true },
@@ -165,15 +178,12 @@ export default {
         { text: "First Name", value: "firstName", divider: true },
         { text: "Last Name", value: "lastName", divider: true },
         { text: "Email", value: "email", divider: true },
-        { text: "Actions", value: "actions", sortable: false },
+        { value: "actions", sortable: false },
       ],
 
       emailRules: [
         (v) => !!v || "E-mail is required",
-        (v) =>
-          /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-            v
-          ) || "E-mail must be valid",
+        (v) => this.pattern.test(v) || "E-mail must be valid",
       ],
       nameRules: [(v) => !!v || "Name is required"],
 
@@ -193,18 +203,9 @@ export default {
     };
   },
 
-  watch: {
-    userDialog: function (newValue) {
-      if (!newValue) {
-        this.userDialog = newValue;
-        this.editedItem = this.defaultItem;
-      }
-    },
-  },
-
   methods: {
     createNewUser() {
-      this.userDialog = true;
+      this.checkUserDialog = true;
       this.isCreating = true;
     },
 
@@ -223,7 +224,7 @@ export default {
 
     editUser(user) {
       this.editedItem = user;
-      this.userDialog = true;
+      this.checkUserDialog = true;
       this.isCreating = false;
     },
 
@@ -235,8 +236,9 @@ export default {
             (item) => response.data.id === item.id
           );
           this.inputAccounts[index] = this.editedItem;
+
+          this.checkUserDialog = false;
         });
-      this.userDialog = false;
     },
 
     removeUser() {
@@ -251,24 +253,10 @@ export default {
     },
 
     saveAccout() {
-      if (
-        this.editedItem.firstName &&
-        this.editedItem.login &&
-        this.editedItem.lastName &&
-        this.editedItem.email
-      ) {
-        axios
-          .post("http://localhost:7777/", this.editedItem)
-          .then((response) => {
-            this.inputAccounts.push(response.data);
-            this.editedItem = this.defaultItem;
-            this.userDialog = false;
-            this.validForm = false;
-          });
-      } else {
-        this.validForm = true;
-        return;
-      }
+      axios.post("http://localhost:7777/", this.editedItem).then((response) => {
+        this.inputAccounts.push(response.data);
+        this.checkUserDialog = false;
+      });
     },
   },
 };
